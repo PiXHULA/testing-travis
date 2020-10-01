a962942fcaa5e95b95a0e011c012b9e960bfe2de
Joakim Briceno <joakim.onnhage@gmail.com>
2020-10-01 20:15:36 +0200

    Trying to set up tests for repo

--- src/test/java/com/example/demo/RepositoryTest.java
+++ src/test/java/com/example/demo/RepositoryTest.java
@@ -4,15 +4,11 @@ import com.example.demo.domain.Application;
 import com.example.demo.domain.Package;
 import com.example.demo.repository.ApplicationRepository;
 import com.example.demo.repository.PackageRepository;
-import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 
 
 import java.util.ArrayList;
@@ -23,11 +19,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 public class RepositoryTest {
-    Application app1 = Application.builder().Id(1L).link("http://1").logo("Logo1").build();
-    Application app2 = Application.builder().Id(2L).link("http://2").logo("Logo2").build();
 
-    List<Application> list = new ArrayList<>();
-    Package pack1 = Package.builder().applications(list).build();
+    List<Application> applicationList = new ArrayList<>();
+    List<Package> packageList = new ArrayList<>();
+    Application app1 = Application.builder().Id(1L).link("http://1").packageList(packageList).logo("Logo1").build();
+    Application app2 = Application.builder().Id(2L).link("http://2").packageList(packageList).logo("Logo2").build();
+    Package pack1 = Package.builder().Id(1L).applications(applicationList).build();
     Package pack2;
 
     @Autowired
@@ -41,10 +38,11 @@ public class RepositoryTest {
 //    }
     @BeforeEach
     public void addItemsToDB(){
+        packageList.add(pack2);
         applicationRepository.save(app1);
         applicationRepository.save(app2);
-        list.add(app1);
-        list.add(app2);
+        applicationList.add(app1);
+        applicationList.add(app2);
         pack2 = packageRepository.save(pack1);
     }
 
@@ -63,17 +61,29 @@ public class RepositoryTest {
     }
 
     @Test
+    public void deleteApplicationFromDB(){
+        applicationRepository.delete(app1);
+        assertThat(applicationRepository.findById(1L)).isEmpty();
+    }
+
+    @Test
     public void deletePackageAndKeepApplicationsInDB(){
         packageRepository.delete(pack1);
         assertThat(applicationRepository.findAll()).hasSize(2);
         assertThat(applicationRepository.findById(1L)).isNotNull();
     }
 
-    @Test
+    //@Test
     @DisplayName("Delete an application and make sure it removes from the package")
     public void deleteApplicationAndNotShowInPackages(){
-        //assertThat(pack1.getApplications()).hasSize(2);
+
+        Package newPackage = packageRepository.getOne(1L);
+        assertThat(newPackage.getApplications()).hasSize(2);
+
         applicationRepository.delete(app1);
-        assertThat(pack1.getApplications()).hasSize(1);
+        List <Application> allItems = applicationRepository.findAll();
+        assertThat(allItems.get(0).getId()).isEqualTo(2L);
+//        Package newPackage2 = packageRepository.getOne(1L);
+//        assertThat(newPackage2.getApplications()).hasSize(1);
     }
 }
