1eff30e471e400de9442bf0c8fa3961887624642
Joakim Briceno <joakim.onnhage@gmail.com>
2020-10-05 12:20:37 +0200

    All testst done

--- src/test/java/com/example/demo/RepositoryTest.java
+++ src/test/java/com/example/demo/RepositoryTest.java
@@ -10,24 +10,30 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+
 @DataJpaTest
 public class RepositoryTest {
+    Set<Application> applicationSet1 = new HashSet<>();
+    Set<Application> applicationSet2 = new HashSet<>();
+    Set<Package> packageSet = new HashSet<>();
 
-    List<Application> applicationList = new ArrayList<>();
     Application app1 = Application.builder().Id(1L).link("http://1").logo("Logo1").build();
     Application app2 = Application.builder().Id(2L).link("http://2").logo("Logo2").build();
-    Package pack1 = Package.builder().Id(1L).applications(applicationList).build();
-    Package pack2 = Package.builder().Id(2L).applications(applicationList).build();
-    List<Application> fetchedList;
+    Application app3 = Application.builder().Id(3L).link("http://2").logo("Logo2").build();
+    Application app4 = Application.builder().Id(4L).link("http://2").logo("Logo2").build();
+    Package pack1 = Package.builder().Id(1L).build();
+    Package pack2 = Package.builder().Id(2L).build();
+
+    Application app5;
+    Application app6;
+
     Package pack3;
-    Package pack4;
+
 
     @Autowired
     ApplicationRepository applicationRepository;
@@ -36,72 +42,75 @@ public class RepositoryTest {
 
     @BeforeEach
     public void addItemsToDB(){
-        applicationRepository.save(app1);
-        applicationRepository.save(app2);
-        fetchedList = applicationRepository.findAll();
-        applicationList.add(app1);
-        applicationList.add(app2);
-        pack3 = packageRepository.save(pack1);
-        pack4 = packageRepository.save(pack2);
+       app5 = applicationRepository.save(app1);
+       app6 = applicationRepository.save(app2);
+       applicationRepository.save(app3);
+       pack1.addApplication(app1);
+       pack1.addApplication(app2);
+       pack1.addApplication(app3);
+       pack2.addApplication(app1);
+       pack2.addApplication(app2);
+       pack3 = packageRepository.save(pack1);
     }
 
     @Test
     @DisplayName("Checking if applications has been added to Database")
     public void checkIfApplicationsBeenAddedToDb(){
         List <Application> allItems = applicationRepository.findAll();
-        assertThat(allItems).hasSize(2);
+        assertThat(allItems).hasSize(3);
     }
 
     @Test
     @DisplayName("Checking if one application belongs to a package")
     public void checkIfApplicationsBelongsToPackage(){
-         assertThat(pack2.getApplications()).hasSize(2);
-         Application app3 = pack2.getApplications().get(0);
-         Application app4 = applicationRepository.getOne(1L);
-         assertThat(app3.getLogo()).isEqualTo(app4.getLogo());
+         assertThat(pack1.getApplications().contains(app1)).isTrue();
+         assertThat(pack1.getApplications().contains(app4)).isFalse();
     }
+
     @Test
     @DisplayName("Checking if Application can belong to more than one package")
     public void checkIfApplicationsBelongsToMoreThanOnePackage(){
-         assertThat(pack1.getApplications()).hasSize(2);
-         assertThat(pack2.getApplications()).hasSize(2);
-
-         Application appFromPack1 = pack1.getApplications().get(0);
-         Application appFromPack2 = pack2.getApplications().get(0);
-         Application appFromRepo = applicationRepository.getOne(1L);
-
-         assertThat(appFromPack1.getLogo()).isEqualTo(appFromRepo.getLogo());
-         assertThat(appFromPack2.getLogo()).isEqualTo(appFromRepo.getLogo());
+         //PACK1
+         assertThat(pack1.getApplications().contains(app1)).isTrue();
+         assertThat(pack1.getApplications().contains(app2)).isTrue();
+         assertThat(pack1.getApplications().contains(app3)).isTrue();
+         //PACK2
+         assertThat(pack2.getApplications().contains(app1)).isTrue();
+         assertThat(pack2.getApplications().contains(app2)).isTrue();
+         assertThat(pack2.getApplications().contains(app3)).isFalse();
     }
 
     @Test
     @DisplayName("Deleting application from DB")
     public void deleteApplicationFromDB(){
-        assertThat(fetchedList).hasSize(2);
-//        applicationRepository.deleteById(1L);
-//        applicationRepository.flush();
+        //APP1 Exists
+        assertThat(applicationRepository.findById(1L)).isNotNull();
 
-        assertThat(fetchedList).hasSize(2);
-        assertThat(fetchedList.get(1)).isNotNull();
+        //REMOVING APP1
+        applicationRepository.delete(app1);
+        assertThat(applicationRepository.findById(1L)).isEmpty();
+        assertThat(applicationRepository.findById(2L)).isNotNull();
+        assertThat(applicationRepository.findById(3L)).isNotNull();
     }
 
     @Test
     @DisplayName("Delete a package and keep the applications in database")
     public void deletePackageAndKeepApplicationsInDB(){
         packageRepository.delete(pack1);
-        assertThat(applicationRepository.findAll()).hasSize(2);
+        packageRepository.delete(pack2);
+
         assertThat(applicationRepository.findById(1L)).isNotNull();
+        assertThat(applicationRepository.findById(2L)).isNotNull();
+        assertThat(applicationRepository.findById(3L)).isNotNull();
     }
 
     @Test
     @DisplayName("Delete an application and make sure it removes from the package")
     public void deleteApplicationAndNotShowInPackages(){
-        Package newPackage = packageRepository.getOne(1L);
-        assertThat(newPackage.getApplications()).hasSize(2);
         applicationRepository.delete(app1);
-        newPackage = packageRepository.getOne(1L);
-        assertThat(newPackage.getApplications()).hasSize(2);
-        assertThat(newPackage.getApplications().get(0)).isNotNull();
-        assertThat(newPackage.getApplications().get(1)).isNotNull();
+        pack1.deleteApplication(app1);
+        assertThat(pack1.getApplications().contains(app1)).isFalse();
+
+
     }
 }
